3b9606afb8fadd496698b921dacacdd51d3a63b4
S-Henderson <52982548+S-Henderson@users.noreply.github.com>
2020-10-15 17:26:43 -0400

    Add files via upload

--- scripts/reporting.R
+++ scripts/reporting.R
@@ -1,52 +1,42 @@
-# Build summary workbook
+# Build summary workbook #
   
-# Prep workbook
+# Prep workbook -----
 
 wb <- createWorkbook()
 
-# Add sheets
+# Add sheets -----
 
-addWorksheet(
-  wb, 
-  sheetName = "Program Type Count", 
-  tabColour = "#E6B8B7"             # Red
-)
+addWorksheet(wb, 
+             sheetName = "Program Type Count", 
+             tabColour = "#E6B8B7")            # Red
 
-addWorksheet(
-  wb, 
-  sheetName = "Status Count",       
-  tabColour = "#D8E4BC"             # Green
-)
 
-addWorksheet(
-  wb, 
-  sheetName = "Client Count",       
-  tabColour = "#FFE699"             # Yellow
-)
+addWorksheet(wb, 
+             sheetName = "Status Count",       
+             tabColour = "#D8E4BC")           # Green
 
-# Write data
 
-writeData(
-  wb, 
-  sheet = "Program Type Count", 
-  x = df_program_type_count(), # Program Type Count
-)
+addWorksheet(wb, 
+             sheetName = "Client Count",       
+             tabColour = "#FFE699")           # Yellow
 
-writeData(
-  wb, 
-  sheet = "Status Count", 
-  x = df_status_count(),       # Status Count
-)
 
-writeData(
-  wb, 
-  sheet = "Client Count", 
-  x = df_client_count(),       # Client Count
-)
+# Write data -----
 
-# Save workbook
+writeData(wb, 
+          sheet = "Program Type Count", 
+          x = df_program_type_count(),) # Program Type Count
 
-saveWorkbook(
-  wb,
-  file = download_file
-)
+
+writeData(wb, 
+          sheet = "Status Count", 
+          x = df_status_count(),)       # Status Count
+
+writeData(wb, 
+          sheet = "Client Count", 
+          x = df_client_count(),)       # Client Count
+
+# Save workbook -----
+
+saveWorkbook(wb,
+             file = download_file)
